d3a63132adeae01a13ed42749e8be74cf44ee8c3
tivt <tivt231@gmail.com>
2023-07-05 18:06:48 -0300

    deploy changes

--- client/src/api/axios.ts
+++ client/src/api/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://todo-frontend-mentor-challenge-server.onrender.com",
 });
 
 export const addTokenToRequest = () => {
